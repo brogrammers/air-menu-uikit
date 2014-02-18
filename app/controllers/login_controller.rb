2ceaa7428c826002f74bbe416c23fc441e497916
Max Hoffmann <tsov@me.com>
2014-02-18 14:43:45 +0000

    added rescue block for invalid cert

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,18 +1,18 @@
 require 'oauth2'
 require 'json'
+require 'openssl'
 
 class LoginController < ApplicationController
 
   def create
-    puts AirMenu::Settings.client_id
-    puts AirMenu::Settings.client_secret
     scope_parameter = AirMenu::Settings.scopes.join ' '
     access_token = client.password.get_token(params[:username], params[:password], :scope => scope_parameter)
     session[:access_token] = access_token.to_hash[:access_token]
-    puts session
     render :json => { :message => 'ok' }
   rescue OAuth2::Error
     render :json => { :error => 'wrong_credentials' }, :status => :unauthorized
+  rescue Faraday::SSLError
+    render :json => { :error => 'invalid_cert' }, :status => :unauthorized
   end
 
   def index
